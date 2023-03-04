22f3b0f30cba445698eae902869933d221ed70a3
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2023-03-04 12:57:22 -0300

    Camada Application - Parte 2

---
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -0,0 +1,49 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class ProjectService : IProjectService
+    {
+        int IProjectService.Create(NewProjectInputModel newProjectInputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProjectService.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProjectService.Finish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ProjectViewModel> IProjectService.GetAll(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        ProjectDetailsViewModel IProjectService.GetAll(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProjectService.Started(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProjectService.Update(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevFreela.Infraestructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,38 @@
+﻿using DevFreela.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>
+            {
+                new Project("Projeto teste1", "Descrição", 1, 1, 10000),
+                new Project("Projeto teste2", "Descrição", 1, 1, 10000),
+                new Project("Projeto teste3", "Descrição", 1, 1, 10000),
+            };
+            Users = new List<User>
+            {
+                new User("Vinicius", "email@gmail.com", new DateTime(2000, 25, 09)),
+                new User("Vinicius", "email@gmail.com", new DateTime(2000, 25, 09)),
+                new User("Vinicius", "email@gmail.com", new DateTime(2000, 25, 09)),
+            };
+            Skills = new List<Skill>
+            {
+                new Skill("C#"),
+                new Skill("Js"),
+                new Skill("Ts"),
+            };
+        }
+
+        public List<Project> Projects { get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
